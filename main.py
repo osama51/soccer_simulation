46d791b938fe2ec50f75dbe144b7e7412fdbca95
osama51 <m.osos2011@gmail.com>
2022-05-19 08:04:42 +0200

    added comments

--- main.py
+++ main.py
@@ -47,60 +47,100 @@ class MainApp(QMainWindow, FORM_CLASS):
         
         
     def ball_elevations(self):
-        # velocity_x = v0 * math.cos(theta)
-        # velocity_y = v0 * math.sin(theta) - self.gravity * elapsed_t
+    
+        """" 
+        velocity_x = v0 * math.cos(theta)
+        velocity_y = v0 * math.sin(theta) - self.gravity * elapsed_t 
+        
+        theta: the kick angle
+        v0: The initial velocity
+        distance_s: distance from start to goal plane 
+        elapsed_t1: The horizontal distance between the kicking point and the goal plane
+        elapsed_t2: The horizontal distance between the goal plane and the maximum elevation point
+        t: range of total time (0 : total time) with a step of 0.01
+        """
+        
+        #####################################################
+        #                   Control Knobs                   #
+        #####################################################
         
         theta = self.doubleSpinBox_theta.value()
         theta = math.radians(theta)
         v0 = self.doubleSpinBox_v.value()
         distanc_s = self.doubleSpinBox_s.value()
         
-        elapsed_t1 = (v0 * math.sin(theta))/self.gravity
+        #####################################################
+        #                 Height Calculation                #
+        #####################################################
+       
+        elapsed_t1 = (v0 * math.sin(theta))/self.gravity  
         print("T", elapsed_t1)
+
         self.max_height = (v0 * elapsed_t1 * math.sin(theta)) - (0.5 * self.gravity * (elapsed_t1**2))
+        
         distance_maxh = v0 * elapsed_t1 * math.cos(theta)
         print("S", distance_maxh)
-        elapsed_t2 = (distanc_s - distance_maxh)/(v0 * math.cos(theta))
+        
+        elapsed_t2 = (distanc_s - distance_maxh) / (v0 * math.cos(theta))
         print("Tao", elapsed_t2,'\n')
+        
         self.goal_height = self.max_height - (0.5 * self.gravity * (elapsed_t2**2))
         
         if self.goal_height < 0: self.goal_height = 0
         
-        self.labelMaxHeight.setText(str(round(self.max_height,2))+" m")
-        self.labelGoalHeight.setText(str(round(self.goal_height,2))+" m")
-        x = np.arange(0,np.pi,0.1)
+        self.labelMaxHeight.setText(str(round(self.max_height,2))+" m")   # height units
+        self.labelGoalHeight.setText(str(round(self.goal_height,2))+" m")      
         
-        
-        t =  np.arange(0,(elapsed_t1+elapsed_t2),0.01)
+        t =  np.arange(0,(elapsed_t1+elapsed_t2),0.01)        
         x = v0 * t * np.cos(theta)
         y = v0 * t * np.sin(theta) - (0.5 * self.gravity * t**2)
-       
-        
+
         for index, value in enumerate(y):
-            
             if value < 0:
                 y[index] = 0
         
-        
+        """ testing (don't bother)"""
         # self.image = cv2.imread("images/grass.jpg")
         # self.image = cv2.cvtColor(self.image, cv2.COLOR_BGR2RGB)
         # img = pg.ImageItem( image=self.image )
         # plott = pg.PlotItem.addItem.plot(x, y, pen= self.dotted_pen)
-        
         
         self.graphicsView.clear()
         # self.graphicsView.setBackgroundBrush(QBrush(QImage("images/grass.jpg")))
         # self.graphicsView.setCacheMode(QGraphicsView.CacheBackground)
         self.graphicsView.plot(x, y, pen= self.dotted_pen)
         
+        """" At maximum height, velocity_y = 0
+            line 76: We get time1 at that point from velocity_y equation @line 53 """
+        
+        """ line 79: Get max height by integrating velocity_y @line 53 """
+        
+        """ line 81: Get distance from start point to max height point by integrating velocity_x @line 52 """
+        
+        """ The horizontal distance between the goal plane and the maximum height point 
+            equals the integration of velocity_x along that duration 
+            line 84: v0 * t2 *cos(theta) = S - """
+        
+        """ During the falling phase the ball dynamics is described by velocity_y = -gt
+            by integrating that we acquire the effect of gravity in the - y direction
+            line 87: height at goal plane = maximum height - effect of gravity """
+        
+        """ line 89: if height at goal plane is negative, replace it with a zero """
+        
+        """ line 94: x & y axes, velocity equations integrated to get distance equations 
+            (mainly for the graphical curve)"""
+        
+        """ line 98: for every negative height value, replace it with a zero """
+        
+        
     def score_probability(self):
         
-        mu = self.doubleSpinBox_mu.value()
-        std = self.doubleSpinBox_std.value()
-        speed = self.doubleSpinBox_spd.value()
+        mu = self.doubleSpinBox_mu.value()     # mean
+        std = self.doubleSpinBox_std.value()   # standard deviation
+        speed = self.doubleSpinBox_spd.value() # hit speed 
         
-        x = np.linspace(mu - 3*std, mu + 3*std, int(6*std))
-        sub = speed - (mu-(3*std))
+        x = np.linspace(mu - 3*std, mu + 3*std, int(6*std)) 
+        sub = speed - (mu-(3*std)) # the start of the curve on the x axis
         if sub<0: sub = 0
         self.graphicsView_normal.clear()
         self.graphicsView_normal.plot(x, norm.pdf(x, mu, std), pen=self.yellow_pen, brush=(50,50,200,60))
@@ -109,10 +149,18 @@ class MainApp(QMainWindow, FORM_CLASS):
         
         self.labelProbability.setText(str(int(round(cdf,2)*100))+" %")
         
-             
+        """ 
+            x axis: mean in the middle with 3*std on both sides
             
-
+            In the empirical sciences, the so-called three-sigma rule of thumb 
+            (or 3σ rule) expresses a conventional heuristic that nearly all values 
+            are taken to lie within three standard deviations of the mean, and thus 
+            it is empirically useful to treat 99.7% probability as near certainty.
+        """
         
+        
+        
+             
 def main():
     app = QApplication(sys.argv)
     style = """
